665d1c575c0aab1bc726b769b0fc7091983d7d17
Maor <maorsa9@gmail.com>
2023-08-06 17:23:05 +0300

    Added the ability to SignUp new user

--- util/auth.js
+++ util/auth.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-//import {dotenv} from 'dotenv'
+import { API_KEY } from '@env';
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
-      process.env.API_KEY,
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
     {
       email: email,
       password: password,
